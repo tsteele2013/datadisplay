f721e009f4f7783e45d0514a45303e0d6f8f75b1
Trevor Steele <trevor.steele@yahoo.com>
2022-01-23 23:12:27 -0600

    Initial Commit

---
+++ DataDisplay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataDisplay
+//
+//  Created by Trevor Steele on 1/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
